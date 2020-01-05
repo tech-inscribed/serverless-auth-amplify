53cea10d5db508eee36d378e05f827f64492ab5f
Praveen <geeks4share@gmail.com>
2020-01-05 09:48:19 +0530

    added auth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator } from 'aws-amplify-react';
 
 function App() {
   return (
@@ -23,4 +24,36 @@ function App() {
   );
 }
 
-export default App;
+const signUpConfig = {
+  header: 'Create an Account',
+  hideAllDefaults: true,
+  defaultCountryCode: '1',
+  signUpFields: [
+    {
+      label: 'Name',
+      key: 'name',
+      required: true,
+      displayOrder: 1,
+      type: 'string'
+    },
+    {
+      label: 'Email or Phone number',
+      key: 'username',
+      required: true,
+      displayOrder: 2,
+      type: 'string'
+    },
+    {
+      label: 'Password',
+      key: 'password',
+      required: true,
+      displayOrder: 3,
+      type: 'password'
+    }
+  ]
+};
+
+export default withAuthenticator(App, {
+  includeGreetings: true,
+  signUpConfig
+});
